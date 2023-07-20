6b20a93ac256575c28409fefa6e0209acb5a8025
Mariana Santos <maririb51@gmail.com>
2023-07-20 11:08:42 +0100

    OAuth2 e requisição de login

--- src/main/java/com/devsuperior/bds03/entities/Role.java
+++ src/main/java/com/devsuperior/bds03/entities/Role.java
@@ -2,13 +2,13 @@ package com.devsuperior.bds03.entities;
 
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 
 @SuppressWarnings("serial")
